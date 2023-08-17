f0d4d68a5fe54abf021093761488e656e532b8a7
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-08-17 12:46:25 -0800

    ver 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 app.use(express.static(`${__dirname}/public`))
 
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '/public/index.html')) })
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '/public/index.HTML')) })
 
 app.get('/api/loan', (req, res) => {
     res.status(200).send("Congrats on your new loan")
