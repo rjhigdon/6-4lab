43b2e599a61aaf0d8b4893db40d61577856b1656
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-08-17 13:04:29 -0800

    ver 3

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express()
 const path = require('path')
 app.use(express.static(`${__dirname}/public`))
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9fef3d166216492c86b774f64cc0a2cf',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('hello')
+
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '/public/index.HTML')) })
 
 app.get('/api/loan', (req, res) => {
